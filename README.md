78e8d6ce77253adc9916c365e8375aba27289c7c
Panagiotis Anagnostou <panagno@uth.gr>
2023-04-11 18:41:27 +0300

    Initial commit
    Co-Authored-By: ptheochar <81121418+ptheochar@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,97 @@
+AI-GA: AI-Generated Abstracts dataset
+=====================================
+
+The AI-GA (Artificial Intelligence Generated Abstracts) dataset is a collection of abstracts and titles, with half of the abstracts being AI-generated and the other half being original. This dataset is designed to be used for research and experimentation in the field of natural language processing, particularly in the context of language generation and machine learning.
+
+Dataset Description
+-------------------
+
+The AI-GA dataset contains a total of *28662* samples, with each sample consisting of an abstract, a title, and a label. The dataset is split evenly into two categories: "AI-generated abstracts" and "original abstracts". The label indicates whether the sample is an original abstract (labeled as 0) or an AI-generated abstract (labeled as 1). The AI-generated abstracts are generated using state-of-the-art language generation techniques specifically, the GPT-3 model. The original abstracts are sourced from a corpus of existing research papers about the COVID-19 pandemic found on [GitHub](https://github.com/allenai/cord19).
+
+The dataset is provided in a CSV format, with each row representing a single sample. The CSV file contains three columns: `abstract`, `title`, and `label`. The `abstract` column contains the text of the abstract, the `title` column contains the title of the corresponding abstract, and the `label` column contains the label indicating the type of the abstract.
+
+
+Data analsysis
+--------------
+
+The `read_data.py` is a Python script designed to clean and split the AI-GA dataset. It takes the AI-GA dataset as input and performs cleaning operations such as removing special characters, converting text to lowercase, and removing stop words. It then splits the cleaned data into train and test sets for machine learning or natural language processing tasks.
+
+The script supports the following command line arguments:
+
+- `clean`: A boolean argument (True/False) to specify whether to clean the text data. If set to True, the script will perform text cleaning operations. Default is False.
+
+- `split`: A floating point argument (0.0 to 1.0) to specify the ratio for train/test splitting. The script will randomly split the data into train and test sets according to the specified ratio. Default is 0.8, which means 80% train and 20% test.
+
+- `random_state`: An integer argument to specify the random seed for train/test splitting. The script will use this seed to ensure reproducibility of the same train/test split. Default is value of the random state is 42.
+
+The default arguments of this script recreate the data as we used them in our experiments. The final train and test sets are saved as CSV files in the folder of the script.
+
+### Usage
+
+To run the script, you can use the following command:
+
+```{sh}
+python read_data.py [clean=True|False] [split=FLOAT] [random_state=INT]
+```
+
+The dependencies for the script are listed in the requirements.txt file. You can install the dependencies using the following command:
+
+```{sh}
+pip install -r requirements.txt
+```
+
+If you encounter any issues with the script, please create an issue in this repository.
+
+### Examples
+
+1. Run the script with default settings (no cleaning, 80% train and 20% test, no specific random seed):
+
+```{sh}
+python read_data.py
+```
+
+2. Clean the text data and split into 75% train and 25% test sets:
+
+```{sh}
+python read_data.py split=0.75
+```
+
+3. Clean the text data, split into 80% train and 20% test sets, and set the random seed to 123:
+
+Command line execution:
+```{sh}
+python read_data.py clean=True split=0.8 random_state=123
+```
+
+Dataset License
+---------------
+
+The AI-GA dataset is released under the [MIT license](https://github.com/panagiotisanagnostou/AI-GA). Please refer to the LICENSE file in this repository for more details.
+Repository Contents
+
+This repository contains the following files:
+
+- `ai-ga-dataset.csv`: The main dataset file in CSV format, containing the AI-generated and original abstracts.
+- `README.md`: This README file providing information about the dataset and its usage.
+- `LICENSE`: The license file specifying the terms and conditions for using the AI-GA dataset.
+- `script.py`: The Python script used to generate the AI-generated abstracts for the dataset. Please refer to the script for details on the generation process.
+
+Usage Instructions
+------------------
+
+To use the AI-GA dataset, you can download the ai-ga-dataset.csv file and load it into your preferred programming environment or machine learning framework. You can then use the dataset for training and evaluating your machine learning models or for other research purposes.
+
+To generate additional AI-generated abstracts using the script provided in this repository, you can run the script.py script in your Python environment. Please note that you may need to install additional dependencies or modify the script to suit your specific use case.
+
+Citation
+--------
+
+If you use the AI-GA dataset in your research or publications, please acknowledge its use by citing this repository [insert citation details].
+
+Contributions
+-------------
+
+Contributions to the AI-GA dataset are welcome! If you would like to contribute to this dataset by providing additional samples or improving the existing dataset, please create a pull request or contact the repository owner for more information.
+Contact Information
+
+For any questions, issues, or feedback regarding the AI-GA dataset or this repository, please contact [insert contact information].
---
+++ read_data.py
@@ -0,0 +1,181 @@
+# MIT License
+#
+# Copyright (c) 2023 Panagiotis Anagnostou
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+    This script reads the AI-GA dataset and splits it into train and test sets.
+    The default split is 80% train and 20% test. The script also has the option
+    to clean the text of the abstracts. The cleaning process is described in the
+    clean_text function and is based on the cleaning process of the AI-GA
+    dataset. The data are separated by the title of the paper, so that the same
+    paper is original and generated abstracts to be in the same set. Finally,
+    the train and test sets are saved as csv files in the same directory, with
+    the name "train.csv" and "test.csv" respectively.
+"""
+
+from tqdm import tqdm
+
+import pandas as pd
+import re
+import stopwordsiso as stopwords
+import sys
+
+
+def clean_text(abstract):
+    """
+    Function for basic text cleaning of the AI-GA dataset. The cleaning we
+    followed steps are:
+
+    1. Remove HTML tags
+    2. Remove -
+    3. Remove punctuation
+    4. Convert to lowercase
+    5. Remove numbers
+    6. Remove stop words
+    7. Remove extra whitespace
+    8. Remove study and paper keywords. These keywords are not useful for the
+    classification task, because we classify scientific papers.
+
+    Parameters
+    ----------
+    abstract : str
+        The text to be cleaned.
+
+    Returns
+    -------
+    text : str
+        The cleaned text.
+
+    """
+    # Remove HTML tags
+    text = re.sub(r'<.*?>', '', abstract)
+
+    # Remove -
+    text = re.sub(r'-', ' ', text)
+
+    # Remove punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove numbers
+    text = re.sub(r'\d', '', text)
+
+    # Remove stop words
+    text = " ".join([word for word in text.split() if word not in stopwords.stopwords("en")])
+
+    # Remove extra whitespace
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    text = re.sub(r'study', ' ', text)
+    text = re.sub(r'paper', ' ', text)
+
+    return text
+
+
+if __name__ == "__main__":
+    # Read the dataset
+    df = pd.read_csv("ai-ga-dataset.csv", engine="c")
+
+    # Initialize the command line arguments
+    clean = False
+    split = 0.8
+    random_state = 42
+    # Acceptable command line arguments
+    command_line_args = ["clean", "split", "random_state"]
+
+    # Check if the user provided any command line arguments and if they have valid names
+    if len(sys.argv) > 4:
+        raise ValueError("Too many arguments")
+    elif len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            if arg.split("=")[0] not in command_line_args:
+                raise ValueError(f"Invalid argument: {arg.split('=')[0]}")
+            else:
+                if arg.split("=")[0] == "clean":
+                    clean = arg.split("=")[1]
+                elif arg.split("=")[0] == "split":
+                    split = arg.split("=")[1]
+                elif arg.split("=")[0] == "random_state":
+                    random_state = arg.split("=")[1]
+
+    # Check if the values of the clean, split and random_state command line arguments are valid
+    try:
+        split = float(split)
+    except ValueError:
+        raise ValueError("split: Invalid split value")
+    if split < 0 or split > 1:
+        raise ValueError("split: Invalid split value")
+
+    if clean:
+        if clean.lower() == "true":
+            clean = True
+        elif clean.lower() == "false":
+            clean = False
+        else:
+            raise ValueError("clean: Invalid clean value")
+
+    try:
+        random_state = int(random_state)
+    except ValueError:
+        raise ValueError("random_state: Invalid random_state value")
+    if random_state < 0:
+        raise ValueError("random_state: Invalid random_state value")
+
+    # Clean the text if the user provided the clean command line argument
+    if clean:
+        print("Cleaning text...")
+        tqdm.pandas()
+        df["abstract"] = df["abstract"].progress_apply(clean_text)
+
+    original = df[df.label == 0]
+    ai = df[df.label == 1]
+
+    percent = split
+    print("Splitting data into train and test sets...")
+    print(f"The train set contains {split * 100}% and the test set contains {100 - split * 100}% of the data")
+
+    # Apply the train split to the original and generated abstracts
+    ai_train = ai.sample(frac=percent, random_state=random_state)
+    # Get the original abstracts that have the same title as the generated abstracts in the train set
+    original_train = original[original.title.isin(ai_train.title)]
+    # Concatenate the original and generated abstracts in the train set
+    train = pd.concat([original_train, ai_train])
+
+    # Apply the test split to the original and generated abstracts
+    ai_test = ai[~ai.title.isin(ai_train.title)]
+    # Get the original abstracts that have the same title as the generated abstracts in the test set
+    original_test = original[original.title.isin(ai_test.title)]
+    # Concatenate the original and generated abstracts in the test set
+    test = pd.concat([original_test, ai_test])
+
+    # Shuffle the train and test sets and reset the index
+    train = train.sample(frac=1).reset_index(drop=True)
+    test = test.sample(frac=1).reset_index(drop=True)
+    print(f"The train set shape is {train.shape}")
+    print(f"The test set shape is {test.shape}")
+
+
+    # Save the train and test sets as csv fil
+    print("Saving train and test sets...")
+    test.to_csv("test.csv", index=False)
+    train.to_csv("train.csv", index=False)
+    print("Done!!!")
